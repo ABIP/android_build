4324f673760703fa169851c518dc3c40ea5f8b62
khusika <khusikadhamar@gmail.com>
2015-04-10 01:35:13 +0700

    Minor updates
    -Set product make files to abip.mk
    -number build: show only AOSP BUILD_ID *Thanks to alviteri
    
    Change-Id: Ib95bbb14a44e8dd407405d1cf821e5f9caceadcf

--- core/product_config.mk
+++ core/product_config.mk
@@ -181,7 +181,7 @@ include $(BUILD_SYSTEM)/device.mk
 
 # A ABIP build needs only the ABIP product makefiles.
 ifneq ($(ABIP_BUILD),)
-  all_product_configs := $(shell ls device/*/$(ABIP_BUILD)/abip_$(ABIP_BUILD).mk)
+  all_product_configs := $(shell ls device/*/$(ABIP_BUILD)/abip.mk)
 else
   ifneq ($(strip $(TARGET_BUILD_APPS)),)
   # An unbundled app build needs only the core product makefiles.
